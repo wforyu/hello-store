54e476fcc187f68f0b6b4f8249141c195ff08184
wforyu <wforyu@users.noreply.github.com>
2026-07-16 21:58:35 +0700

    feat: Firebase push notifications - Gradle config + google-services.json + APK v104
    - Add google-services.json from Firebase (project hello-store-37904)
    - Add com.google.gms.google-services plugin to build.gradle
    - Add Firebase BoM + firebase-messaging + firebase-analytics deps
    - Fix import path in notifications.js (../api/client)
    - Copy google-services.json to android/app/
    - Bump versionCode to 104
    - APK v1.0.0-104 with push notification support

--- mobile/src/utils/notifications.js
+++ mobile/src/utils/notifications.js
@@ -1,7 +1,7 @@
 import * as Notifications from 'expo-notifications';
 import * as Device from 'expo-device';
 import { Platform } from 'react-native';
-import api from './api/client';
+import api from '../api/client';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
